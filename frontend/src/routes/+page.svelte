46f285f87b4bd248ac4451717e76171f9ab5993b
brequet <requet.baptiste@gmail.com>
2025-02-01 22:34:00 +0100

    bundle frontend

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1 class="text-3xl">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="flex flex-col">
+	<a href="about" class="text-blue-500">About</a>
+	<a href="dynamic-route" class="text-blue-500">Dynamic route</a>
+</div>
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_multipart::{form::MultipartFormConfig, test, MultipartError};
+use actix_files::Files;
+use actix_multipart::{form::MultipartFormConfig, MultipartError};
 use actix_web::{web, App, Error, HttpRequest, HttpServer};
 use dotenv;
 use services::parser::TestRun;
@@ -17,16 +18,16 @@ async fn main() -> std::io::Result<()> {
     let pool = config::database::setup_database().await;
 
     // let file_path = "./src/services/resources/output_simplified.xml";
-    let file_path = "./robot-data-sample/8-tests-1-ko/output.xml";
-    let result = services::parser::get_test_run_from_xml(&file_path);
-    match result {
-        Ok(test_run) => {
-            if let Err(e) = services::robot::RobotService::save_test_run(&pool, test_run).await {
-                println!("Ingest error: {:?}", e);
-            }
-        }
-        Err(e) => println!("Error: {:?}", e),
-    }
+    // let file_path = "./robot-data-sample/8-tests-1-ko/output.xml";
+    // let result = services::parser::get_test_run_from_xml(&file_path);
+    // match result {
+    //     Ok(test_run) => {
+    //         if let Err(e) = services::robot::RobotService::save_test_run(&pool, test_run).await {
+    //             println!("Ingest error: {:?}", e);
+    //         }
+    //     }
+    //     Err(e) => println!("Error: {:?}", e),
+    // }
 
     let server_config = config::server::load();
     let addr = format!("127.0.0.1:{}", server_config.port);
@@ -41,6 +42,7 @@ async fn main() -> std::io::Result<()> {
             )
             .configure(routes::ingest::init)
             .configure(|cfg| routes::robot::init(cfg, web::Data::new(pool.clone())))
+            .service(Files::new("/", "./frontend/build").index_file("index.html"))
     })
     .bind(addr)?
     .run()
